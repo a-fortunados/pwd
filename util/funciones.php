574b4ddbbc0d44068ce5cf2f8b5196904523316b
Juan Marcos Gonzalez <juan.gonzalez@est.fi.uncoma.edu.ar>
2021-10-02 15:41:36 -0300

    configuracion y autoload

--- util/funciones.php
+++ util/funciones.php
@@ -19,12 +19,31 @@ function data_submitted()
     return $_AAux;
 
 }
+
 function verEstructura($e)
 {
     echo "<pre>";
     print_r($e);
     echo "</pre>";
 }
+
+spl_autoload_register(function ($clase) {
+    //echo "Se cargo la clase:  ".$clase ;
+    $directorys = array(
+        $_SESSION['ROOT'] . 'modelo/',
+        $_SESSION['ROOT'] . 'modelo/conector/',
+        $_SESSION['ROOT'] . 'control/',
+        //  $GLOBALS['ROOT'].'util/class/',
+    );
+    //print_r($directorys) ;
+    foreach ($directorys as $directory) {
+        if (file_exists($directory . $clase . '.php')) {
+            //  echo "se incluyo".$directory. $class_name . '.php';
+            require_once ($directory . $clase . '.php');
+            return;
+        }
+    }
+});
 
 // function __autoload($class_name){
 //     //echo "class ".$class_name ;
